e03ed98f8b15caceb2a6ec9d27f4798c941cf0f1
Owoade <63651250+Owoade@users.noreply.github.com>
2022-09-10 00:27:55 +0100

    Implemented Article service

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ApplicationRecord
+  validates :title, presence: true
+  validates :body, presence: true, length: {minimum: 10}
+  validates :tags, presence: true, length: { maximum: 20 }
 end
